eb5c74e70ec50d8de31dc585f8b12bff5d40a93c
yaoyao1019 <605455353@qq.com>
2019-04-23 20:32:31 +0800

    测试git

--- src/main/java/top/lfyao/thread/Test1.java
+++ src/main/java/top/lfyao/thread/Test1.java
@@ -1,17 +1,12 @@
 package top.lfyao.thread;
 
 
-import top.lfyao.thread.lock.Reentrant;
-
-import javax.xml.parsers.DocumentBuilder;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Test1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Integer> list = Collections.synchronizedList(new ArrayList<Integer>());
         list.add(2);
         Vector vector = new Vector();
@@ -19,16 +14,19 @@ public class Test1 {
         //  elementData = Arrays.copyOf(elementData, newCapacity);
 
         int[] a = new int[2];
-        a[0]=10;
-        a[1]=11;
+        a[0] = 10;
+        a[1] = 11;
 //        System.out.println(a[2]);
+
         System.out.println(a);
         a = Arrays.copyOf(a, 3);
         System.out.println(a);
-        a[2]=11;
+        a[2] = 11;
         System.out.println(a);
 
+
         CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
+
 
 
     }
